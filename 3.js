6bd582f060ed54d078885417fc7e33010b2ea037
gireeshavallabhaneni <2111cs020137@mallareddyuniversity.ac.in>
2022-12-19 10:50:48 +0530

    first commit

---
+++ 3.js
@@ -0,0 +1,11 @@
+<p id="gireesha">hai</p>
+<button value="submit"onclick="clickme()">submit</button>
+<script>
+    function validateform(){
+        let x=document.forms["myform"]["fname"]
+        if(x=)}
+        function:clickme()
+        {
+        document.getElementbyid("gireesha").innerHTML="hello"
+    }
+    </script>
